1f773a31871a31bc20c484c81b66b4a962636329
Karolina Jaskowska <Karolina.Jaskowska@microsoft.wsei.edu.pl>
2022-03-30 19:22:01 +0200

    test10

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -41,6 +41,19 @@ namespace GradeBook.UserInterfaces
             }
             var name = parts[1];
             BaseGradeBook gradeBook = new BaseGradeBook(name);
+            var type = parts[2];
+            if(type == "standard")
+            {
+                gradeBook = new StandardGradeBook(name);
+            }
+            else if(type == "ranked")
+            {
+                gradeBook = new RankedGradeBook(name);
+            }
+            else
+            {
+                Console.WriteLine(type + " is not a supported type of gradebook, please try again");
+            }
             Console.WriteLine("Created gradebook {0}.", name);
             GradeBookUserInterface.CommandLoop(gradeBook);
         }
