368c9981c11f7cf8b9b298951b2a83d625a4d1a0
Karolina Jaskowska <Karolina.Jaskowska@microsoft.wsei.edu.pl>
2022-03-30 18:36:40 +0200

    Testy

---
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GradeBook.Enums;
+using GradeBook.GradeBooks;
+
+namespace GradeBook.GradeBooks
+{
+    public class StandardGradeBook : BaseGradeBook
+    {
+        public StandardGradeBook (string name): base(name)
+        {
+            Type = GradeBookType.Standard;
+        }
+    }
+}
