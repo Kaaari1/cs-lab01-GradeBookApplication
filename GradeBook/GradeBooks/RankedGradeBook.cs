7441ce4c73121405165f43e87c8aac5d52814f86
Karolina Jaskowska <Karolina.Jaskowska@microsoft.wsei.edu.pl>
2022-03-30 18:39:52 +0200

    Test5

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GradeBook.Enums;
+using GradeBook.GradeBooks;
+namespace GradeBook.GradeBooks
+{
+    class RankedGradeBook : BaseGradeBook
+    {
+        public RankedGradeBook(string name) : base(name)
+        {
+            Type = GradeBookType.Ranked;
+        }
+    }
+}
